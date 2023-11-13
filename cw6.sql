91dcec023ece0fb7d6e3ba86af012a5bd7c9fbce
jakubsumara <96498252+jakubsumara@users.noreply.github.com>
2023-11-13 15:26:23 +0100

    Add files via upload

---
+++ cw6.sql
@@ -0,0 +1,45 @@
+-- 1.0
+CREATE TABLE obiekty
+(
+	nazwa VARCHAR(10),
+	geom geometry
+);
+
+INSERT INTO obiekty VALUES
+('obiekt1', ST_GEOMFROMEWKT('COMPOUNDCURVE( (0 1, 1 1), CIRCULARSTRING(1 1, 2 0, 3 1), CIRCULARSTRING(3 1, 4 2, 5 1), (5 1, 6 1) )')),
+('obiekt2', ST_GEOMFROMEWKT('CURVEPOLYGON( 
+							COMPOUNDCURVE( (10 6, 14 6), CIRCULARSTRING(14 6, 16 4, 14 2), CIRCULARSTRING(14 2, 12 0, 10 2), (10 2, 10 6) ),
+							COMPOUNDCURVE( CIRCULARSTRING(11 2, 12 3, 13 2), CIRCULARSTRING(13 2, 12 1, 11 2) ))')),
+('obiekt3', ST_GEOMFROMEWKT('TRIANGLE( (7 15, 10 17, 12 13, 7 15) )')),
+('obiekt4', ST_GEOMFROMEWKT('MULTILINESTRING( (20 20, 25 25), (25 25, 27 24), (27 24, 25 22), (25 22, 26 21), (26 21, 22 19), (22 19, 20.5 19.5) )')),
+('obiekt5', ST_GEOMFROMEWKT('MULTIPOINT( (30 30 59), (38 32 234) )')),
+('obiekt6', ST_GEOMFROMEWKT('GEOMETRYCOLLECTION( LINESTRING(1 1, 3 2), POINT(4 2))'));
+
+SELECT * FROM obiekty;
+
+-- 1
+
+SELECT ST_AREA(ST_BUFFER(ST_SHORTESTLINE(
+	(SELECT geom FROM obiekty WHERE nazwa='obiekt3'),
+	(SELECT geom FROM obiekty WHERE nazwa='obiekt4') ), 5));
+	
+-- 2
+
+SELECT ST_ISCLOSED((SELECT geom FROM obiekty WHERE nazwa='obiekt4')); -- FALSE
+
+UPDATE obiekty
+SET geom = ST_MAKEPOLYGON(ST_LINEMERGE(ST_COLLECTIONHOMOGENIZE(ST_COLLECT(geom, 'LINESTRING(20.5 19.5, 20 20)'))))
+WHERE nazwa = 'obiekt4';
+
+SELECT ST_ISCLOSED((SELECT geom FROM obiekty WHERE nazwa='obiekt4')); -- TRUE
+
+-- 3
+
+INSERT INTO obiekty VALUES
+('obiekt7', ST_COLLECT((SELECT geom FROM obiekty WHERE nazwa = 'obiekt3'), (SELECT geom FROM obiekty WHERE nazwa = 'obiekt4')));
+
+-- 4 
+
+SELECT ST_AREA(ST_BUFFER(geom, 5)) FROM obiekty WHERE ST_HASARC(obiekty.geom)=FALSE;
+
+SELECT ST_AsPolygon(geom) FROM obiekty;
